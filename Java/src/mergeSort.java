3db9114d663fdc761b3056ebfef41f2899dd80ce
HCABurns <harrycaburns@gmail.com>
2023-03-18 19:52:29 +0000

    Fixing Java Merge Sort.
    Merge sort has now been fixed. The issue was the way I was converting the array to left and right array.

--- Java/src/mergeSort.java
+++ Java/src/mergeSort.java
@@ -1,43 +1,37 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 public class mergeSort {
 
-    public int[] sort(int[] arr){
+    public void sort(int[] arr){
+        if (arr.length < 2) {
+            return;
+        }
+        int mid = arr.length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[arr.length - mid];
+        for (int i = 0; i < mid; i++) {
+            left[i] = arr[i];
+        }
+        for (int i = mid; i < arr.length; i++) {
+            right[i - mid] = arr[i];
+        }
+        sort(left);
+        sort(right);
 
-        //Divide
-        if (arr.length <= 1){
-            return arr;
-        }
-        int mid = (int) Math.floor(arr.length/2);
-        System.out.println(mid);
-        int[] left = sort(Arrays.copyOfRange(arr,0,mid));
-        int[] right = sort(Arrays.copyOfRange(arr,mid+1,arr.length));
-        System.out.println(Arrays.toString(left) + " " + Arrays.toString(right));
-        //Conquer
-        int i =0;
-        int j =0;
-        int k = 0;
-        while (i<left.length && j<right.length){
-            if(left[i] <= right[j]){
-                arr[k] = left[i];
-                i+=1;
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                arr[k++] = left[i++];
             }
-            else{
-                arr[k] = right[j];
-                j+=1;
+            else {
+                arr[k++] = right[j++];
             }
-            k+=1;
         }
-        while (i<left.length){
-            arr[k] = left[i];
-            i+=1;
+        while (i < left.length) {
+            arr[k++] = left[i++];
         }
-        while (j<right.length){
-            arr[k] = left[j];
-            j+=1;
+        while (j < right.length) {
+            arr[k++] = right[j++];
         }
-        System.out.println((Arrays.toString(arr)));
-        return arr;
     }
 
 
@@ -45,7 +39,9 @@ public class mergeSort {
     public static void main(String[] args) {
 
         mergeSort mergeSort = new mergeSort();
-        mergeSort.sort(new int[]{3,6,3,2,6,8,4,2,1});
+        int[] arr = new int[]{3,6,3,2,6,8,4,2,1};
+        mergeSort.sort(arr);
+        System.out.println(Arrays.toString(arr));
 
 
     }
