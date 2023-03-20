90b0a72977aee9b1d456d7f07425480f47a9efc7
HCABurns <harrycaburns@gmail.com>
2023-03-20 20:50:48 +0000

    Adding C algorithms.
    Adding bubble sort, bubble sort with early exit and selection sort in the C programming language.

---
+++ C/bubbleSort.h
@@ -0,0 +1,12 @@
+//
+// Created by harry on 20/03/2023.
+//
+
+#ifndef C_BUBBLESORT_H
+#define C_BUBBLESORT_H
+
+//bubble sort function
+void bubbleSort(int[],int);
+void bubbleSortEarlyExit(int[],int);
+
+#endif //C_BUBBLESORT_H
---
+++ C/selectionSort.h
@@ -0,0 +1,10 @@
+//
+// Created by harry on 20/03/2023.
+//
+
+#ifndef C_SELECTIONSORT_H
+#define C_SELECTIONSORT_H
+
+void selectionSort(int arr[],int);
+
+#endif //C_SELECTIONSORT_H
